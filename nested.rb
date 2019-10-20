d5c7391d9900ae9a967ca8dd41f3be9bf8fe9e69
Vivian Tran <viviantran94@gmail.com>
2019-10-20 10:43:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:lan
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 end
